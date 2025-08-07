2ee93792fbbe276ac20af401f0dfb2919cfd8936
waswayne <oluwese4life@gmail.com>
2025-08-07 16:32:44 +0100

    Add deleteIssue and update Issue controllter and route with test

--- controllers/issueController.js
+++ controllers/issueController.js
@@ -65,3 +65,41 @@ export const getIssueById = async (req, res) => {
       .json({ message: "Error retrieving the issue", error: error.message });
   }
 };
+
+//update issue by id
+export const updateIssue = async (req,res) => {
+    try {
+       const updateIssue = await Issue.findByIdAndUpdate(
+        req.params.id.trim(), req.body, { new: true, runValidators: true }
+    ).populate('reportedBy', 'name email');
+
+    if (!updateIssue) {
+        return res.status(404).json({message: 'Issue not found'})
+    }
+
+    res.status(200).json({ issue: updateIssue })
+    } catch (error) {
+        res.status(500).json({
+            message: "Error updating issue",
+            error: error.message
+        })
+    }
+}
+
+//delete issue by Id
+export const deleteIssue = async (req,res) => {
+    try {
+        const deleteIssue = await Issue.findByIdAndDelete(req.params.id.trim())
+
+        if (!deleteIssue) {
+            return res.status(404).json({message: 'Issue noy found'})
+        }
+
+        res.status(200).json({ message: 'Issue deleted succeesfully'})
+    } catch (error) {
+        res.status(500).json({
+            message: 'Error deleting issue',
+            error: error.message
+        })
+    }
+}
--- routes/issueRoutes.js
+++ routes/issueRoutes.js
@@ -1,14 +1,13 @@
-import express from 'express'
-import { createIssue, getAllIssues } from '../controllers/issueController.js'
-import { getIssueById } from '../controllers/issueController.js'
-import { requireAuth } from '../middleware/authMiddleware.js'
+import express from "express";
+import { createIssue, deleteIssue, getAllIssues, getIssueById, updateIssue, } from "../controllers/issueController.js";
+import { requireAuth } from "../middleware/authMiddleware.js";
 
+const router = express.Router();
 
+router.post("/issues", requireAuth, createIssue);
+router.get("/issues", requireAuth, getAllIssues);
+router.get("/issues/:id", requireAuth, getIssueById);
+router.put('/issues/:id', updateIssue)
+router.delete('/issues/:id', deleteIssue)
 
-const router= express.Router()
-
-router.post('/issues', requireAuth, createIssue)
-router.get('/issues', requireAuth, getAllIssues)
-router.get('/issues/:id', requireAuth, getIssueById)
-
-export default router
+export default router;
